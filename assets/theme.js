dfdf4e492a93631a3b8d7d1e048295fdaf735479
Dmytro733 <ddp0496@gmail.com>
2021-11-14 17:20:55 +0200

    Replaced checkbox --> quantity. Added functionality adding accessories from window popup to cart

--- assets/theme.js
+++ assets/theme.js
@@ -2306,28 +2306,31 @@
                 _this2.miniCartPopupElement.innerHTML = tempElement.querySelector('.mini-cart-popup').innerHTML;
                 _this2.element.dispatchEvent(new CustomEvent('cart:rerendered'));
 
-                let checkArr = _this2.miniCartPopupElement.querySelectorAll('#checked-discount');
-                let arrCheckbox = [];
+                let quantitySelectors = _this2.miniCartPopupElement.querySelectorAll('.quantity-selector--product');
 
-                checkArr.forEach((item) => {
-                  item.addEventListener('click', (event) => {
-                    let target = event.target;
+                fetch('/cart.js', {
+                  credentials: 'same-origin',
+                  method: 'GET',
+                  headers: {
+                    'Cache-Control': 'no-cache'
+                  }
+                }).then(function (content) {
+                  content.json().then(function (cartObj) {
+                    let itemsCartArr = cartObj['items'];
+                    new QuantityPickerAccessoriesPopup(quantitySelectors, _this2);
 
-                    if(target.checked){
-                      arrCheckbox.push(target);
-                      _this2._addAccessoriesFromPopup(item, _this2);
+                    quantitySelectors.forEach((quantitySelector) => {
+                      let quantityTarget = quantitySelector.querySelector('[name="quantity"]');
 
-                      _this2.miniCartPopupElement.querySelector('.add-accessories').style.display = 'block';
-                      _this2.miniCartPopupElement.querySelector('.view-cart').style.display = 'none';
-                    }else{
-                      arrCheckbox.splice(target, 1);
-                      if(arrCheckbox.length == 0){
-                        _this2.miniCartPopupElement.querySelector('.add-accessories').style.display = 'none';
-                        _this2.miniCartPopupElement.querySelector('.view-cart').style.display = 'block';
-                      }
-                      _this2._rmAccessoriesWithPopup(item, _this2);
-                    }
-                  });
+                      itemsCartArr.forEach((itemCart) => {
+                        let productJsonElementAccessories = quantitySelector.querySelector('[data-product-json-accessories]');
+                        let jsonDataAccessories = JSON.parse(productJsonElementAccessories.innerHTML);
+                        if(itemCart['id'] == jsonDataAccessories['product-accessories']){
+                          quantityTarget.value = itemCart['quantity'];
+                        }
+                      })
+                    })
+                  })
                 })
 
               } else {
@@ -2353,125 +2356,7 @@
           });
         });
       }
-    }, {
-      key: "_addAccessoriesFromPopup",
-      value: function _addAccessoriesFromPopup(item, context){
-        let _this = context;
-        let itemsArr = [];
-
-        let productJsonElementAccessories = item.parentNode.querySelector('[data-product-json-accessories]');
-        let jsonDataAccessories = JSON.parse(productJsonElementAccessories.innerHTML);
-
-        fetch('/cart.js', {
-          credentials: 'same-origin',
-          method: 'GET',
-          headers: {
-            'Cache-Control': 'no-cache'
-          }
-        }).then(function (content) {
-
-          content.json().then(function (cartObj) {
-            let items = cartObj['items'];
-
-            items.every((i) => {
-              if(i['id'] == jsonDataAccessories['product-accessories']){
-                fetch('/cart/change.js', {
-                  body: JSON.stringify({
-                    id: JSON.stringify(jsonDataAccessories['product-accessories']),
-                    quantity: i['quantity'] + 1
-                  }),
-                  credentials: 'same-origin',
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest' // This is needed as currently there is a bug in Shopify that assumes this header
-                  }
-                }).then(function(){
-                  _this.itemCount = _this.itemCount + 1;
-
-                  _this._rerender(false).then(function () {
-                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
-                  });
-                })
-                
-              }else{
-
-                itemsArr.push({
-                  'id': JSON.stringify(jsonDataAccessories['product-accessories']),
-                  'quantity': 1
-                })
-
-                let productDataAccess = {
-                  'items': itemsArr
-                };
-
-                fetch('/cart/add.js', {
-                  body: JSON.stringify(productDataAccess),
-                  method: 'POST',
-                  credentials: 'same-origin', 
-                  headers: {
-                  'Content-Type': 'application/json',
-                  'X-Requested-With': 'XMLHttpRequest'
-                  }
-                }).then(function(){
-                  _this.itemCount = _this.itemCount + 1;
-
-                  _this._rerender(false).then(function () {
-                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
-                  });
-                })
-              }
-
-            })
-          })
-        })
-
-      }
-    }, {
-      key: "_rmAccessoriesWithPopup",
-      value: function _rmAccessoriesWithPopup(item, context){
-        let _this2 = context;
-
-        let productJsonElementAccessories = item.parentNode.querySelector('[data-product-json-accessories]');
-        let jsonDataAccessories = JSON.parse(productJsonElementAccessories.innerHTML);
-
-        fetch('/cart.js', {
-          credentials: 'same-origin',
-          method: 'GET',
-          headers: {
-            'Cache-Control': 'no-cache'
-          }
-        }).then(function (content) {
-
-          content.json().then(function (cartObj) {
-            let items = cartObj['items'];
-
-            items.forEach((i) => {
-              if(i['id'] == jsonDataAccessories['product-accessories']){
-                fetch('/cart/change.js', {
-                  body: JSON.stringify({
-                    id: JSON.stringify(jsonDataAccessories['product-accessories']),
-                    quantity: i['quantity'] - 1
-                  }),
-                  credentials: 'same-origin',
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest' // This is needed as currently there is a bug in Shopify that assumes this header
-                  }
-                }).then(function(){
-                  _this2.itemCount = _this2.itemCount - 1;
-
-                  _this2._rerender(false).then(function () {
-                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
-                  });
-                })
-              }
-            })
-          })
-        })
-      }
-    }, {
+    },{
       key: "_checkMiniCartScrollability",
       value: function _checkMiniCartScrollability() {
         var miniCartItemList = this.miniCartElement.querySelector('.mini-cart__line-item-list');
@@ -13385,6 +13270,169 @@
     }
 
     return QuantityPickerAccessories;
+  }();
+
+  var QuantityPickerAccessoriesPopup = /*#__PURE__*/function () {
+    function QuantityPickerAccessoriesPopup(element, context) {
+      _classCallCheck(this, QuantityPickerAccessoriesPopup);
+
+      if (!element) {
+        return;
+      }
+
+      if(element[0] != undefined){
+        element.forEach((item) => {
+          item.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target.parentElement.classList.contains('quantity-selector--product')) {
+              let attribute = target.getAttribute("data-action");
+              let targetParentElement = target.parentNode;
+              this.element = targetParentElement;
+              this.inputElement = this.element.querySelector('[name="quantity"]');
+  
+              if(attribute == "increase-picker-quantity"){
+                onIncrease(this.inputElement, item, context);
+              }else if(attribute == "decrease-picker-quantity") {
+                onDecrease(this.inputElement, item, context);
+              }else{
+                return;
+              }
+            }
+          });
+  
+          item.addEventListener('keyup', (e) => {
+            let target = e.target;
+            if (target.parentElement.classList.contains('quantity-selector--product')) {
+              let targetParentElement = target.parentNode;
+              this.element = targetParentElement;
+              this.inputElement = this.element.querySelector('[name="quantity"]');
+  
+              onInputValueChanged(this.inputElement);
+            }
+          })
+        });  
+      }  
+
+      function onIncrease(input, item, context) {
+        input.value = parseInt(input.value) + 1;
+
+        let _this = context;
+        let itemsArr = [];
+        let productJsonElementAccessories = item.querySelector('[data-product-json-accessories]');
+        let jsonDataAccessories = JSON.parse(productJsonElementAccessories.innerHTML);
+
+        fetch('/cart.js', {
+          credentials: 'same-origin',
+          method: 'GET',
+          headers: {
+            'Cache-Control': 'no-cache'
+          }
+        }).then(function (content) {
+
+          content.json().then(function (cartObj) {
+            let items = cartObj['items'];
+
+            items.every((i) => {
+              if(i['id'] == jsonDataAccessories['product-accessories']){
+                fetch('/cart/change.js', {
+                  body: JSON.stringify({
+                    id: JSON.stringify(jsonDataAccessories['product-accessories']),
+                    quantity: i['quantity'] + 1
+                  }),
+                  credentials: 'same-origin',
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest' // This is needed as currently there is a bug in Shopify that assumes this header
+                  }
+                }).then(function(){
+                  _this.itemCount = _this.itemCount + 1;
+
+                  _this._rerender(false).then(function () {
+                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
+                  });
+                })
+                
+              }else{
+
+                itemsArr.push({
+                  'id': JSON.stringify(jsonDataAccessories['product-accessories']),
+                  'quantity': 1
+                })
+
+                let productDataAccess = {
+                  'items': itemsArr
+                };
+
+                fetch('/cart/add.js', {
+                  body: JSON.stringify(productDataAccess),
+                  method: 'POST',
+                  credentials: 'same-origin', 
+                  headers: {
+                  'Content-Type': 'application/json',
+                  'X-Requested-With': 'XMLHttpRequest'
+                  }
+                }).then(function(){
+                  _this.itemCount = _this.itemCount + 1;
+
+                  _this._rerender(false).then(function () {
+                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
+                  });
+                })
+              }
+
+            })
+          })
+        })
+      }
+
+      function onDecrease(input, item, context) {
+        input.value = Math.max(0, parseInt(input.value) - 1);
+
+        let _this2 = context;
+
+        let productJsonElementAccessories = item.querySelector('[data-product-json-accessories]');
+        let jsonDataAccessories = JSON.parse(productJsonElementAccessories.innerHTML);
+
+        fetch('/cart.js', {
+          credentials: 'same-origin',
+          method: 'GET',
+          headers: {
+            'Cache-Control': 'no-cache'
+          }
+        }).then(function (content) {
+
+          content.json().then(function (cartObj) {
+            let items = cartObj['items'];
+
+            items.forEach((i) => {
+              if(i['id'] == jsonDataAccessories['product-accessories']){
+                fetch('/cart/change.js', {
+                  body: JSON.stringify({
+                    id: JSON.stringify(jsonDataAccessories['product-accessories']),
+                    quantity: i['quantity'] - 1
+                  }),
+                  credentials: 'same-origin',
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest' // This is needed as currently there is a bug in Shopify that assumes this header
+                  }
+                }).then(function(){
+                  _this2.itemCount = _this2.itemCount - 1;
+
+                  _this2._rerender(false).then(function () {
+                    document.dispatchEvent(new CustomEvent('theme:loading:end'));
+                  });
+                })
+              }
+            })
+          })
+        })
+      }
+    }
+
+    return QuantityPickerAccessoriesPopup;
   }();
 
   var ShippingEstimator = /*#__PURE__*/function () {
